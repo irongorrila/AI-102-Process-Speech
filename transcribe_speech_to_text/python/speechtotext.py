66764798f1ae5cc9d3a1be04b44b89966dacedeb
Irongorilla <30632028+irongorrila@users.noreply.github.com>
2020-06-10 17:53:00 +0530

    Update speechtotext.py
    The continuous speech transcription is fixed here. Only part remaining is the concatenation of each utterance into a file

--- transcribe_speech_to_text/python/speechtotext.py
+++ transcribe_speech_to_text/python/speechtotext.py
@@ -2,12 +2,12 @@ import azure.cognitiveservices.speech as speechsdk
 
 # Creates an instance of a speech config with specified subscription key and service region.
 # Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "YourSubscriptionKey", "YourServiceRegion"
+speech_key, service_region = "d90a52b5da2b41debad7b762b08befc7", "centralindia"
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 
 # Creates an audio configuration that points to an audio file.
 # Replace with your own audio filename.
-audio_filename = "transcribe_speech_to_text/media/narration.wav"
+audio_filename = "transcribe_speech_to_text/media/OSR_us_000_0039_8k.wav"
 audio_input = speechsdk.AudioConfig(filename=audio_filename)
 
 # Creates a recognizer with the given settings
@@ -15,21 +15,24 @@ speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audi
 
 print("Recognizing first result...")
 
-# Starts speech recognition, and returns after a single utterance is recognized. The end of a
-# single utterance is determined by listening for silence at the end or until a maximum of 15
-# seconds of audio is processed.  The task returns the recognition text as result. 
-# Note: Since recognize_once() returns only a single utterance, it is suitable only for single
-# shot recognition like command or query. 
-# For long-running multi-utterance recognition, use start_continuous_recognition() instead.
-result = speech_recognizer.recognize_once()
+done = False
+# creating stop event
+def stop_cb(evt):
+    print('CLOSING on {}'.format(evt))
+    speech_recognizer.stop_continuous_recognition()
+    nonlocal done
+    done = True
 
-# Checks result.
-if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-    print("Recognized: {}".format(result.text))
-elif result.reason == speechsdk.ResultReason.NoMatch:
-    print("No speech could be recognized: {}".format(result.no_match_details))
-elif result.reason == speechsdk.ResultReason.Canceled:
-    cancellation_details = result.cancellation_details
-    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-    if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        print("Error details: {}".format(cancellation_details.error_details))
+
+speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+
+speech_recognizer.session_stopped.connect(stop_cb)
+speech_recognizer.canceled.connect(stop_cb)
+
+speech_recognizer.start_continuous_recognition()
+while not done:
+    time.sleep(.5)
